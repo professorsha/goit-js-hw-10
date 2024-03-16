c113afb1059de0c1e22486f936e65e2f69844fb6
professorsha <69015177+professorsha@users.noreply.github.com>
2024-03-16 16:46:54 +0000

    Deploying to gh-pages from @ professorsha/goit-js-hw-10@0e7e1b689059b9cb6456fb2f3fef3e4f4f77f03d 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/styles-161f0875.js";import{i as o}from"./assets/vendor-77e16229.js";const m=document.querySelector(".form");m.addEventListener("submit",e=>{e.preventDefault();const r=e.currentTarget.elements.delay.value,s=e.currentTarget.elements.state.value;new Promise((t,i)=>{setTimeout(()=>{s==="fulfilled"?t():i()},r)}).then(t=>{o.success({color:"green",position:"topCenter",message:`✅ Fulfilled promise in ${r}ms`})}).catch(t=>{o.error({color:"red",position:"bottomCenter",message:`❌ Rejected promise in ${r}ms`})})});
+//# sourceMappingURL=commonHelpers2.js.map
